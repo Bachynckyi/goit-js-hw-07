de35a59a533330384ae85cf2f6a441137296011e
Andrew <bachynckyi.andrii@gmail.com>
2022-07-17 21:41:22 +0300

    task_2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkUp = createImageGallery(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkUp);
+
+
+
+function createImageGallery(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+        <li>
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        </li>`;
+        })
+        .join('');
+};
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionSelector: "img",
+    captionsData: "alt",
+    captionsPosition: "bottom",
+    captionsDelay: 250,
+    showCounter: false,
+    enableKeyboard: true,
+});
